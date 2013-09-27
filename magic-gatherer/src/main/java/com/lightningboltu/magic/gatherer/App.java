987c6541f2bc26844a147c20ff3d470bae698834
Cedric <Cedric@DMDD-W1378>
2013-09-26 21:03:49 -0500

    09/26/13 - base

---
+++ magic-gatherer/src/main/java/com/lightningboltu/magic/gatherer/App.java
@@ -0,0 +1,13 @@
+package com.lightningboltu.magic.gatherer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
